75f9947b21f87fa82e1bc6223009a9bd2ab6c3f2
islam-kamel <islam.kamel@agr.svu.edu.eg>
2020-07-08 00:01:59 +0200

    First Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/whiskey/New Volume/2020/linuxBackup/myprojects/2020/blogger-django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
